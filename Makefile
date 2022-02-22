mnauw/git-remote-hg

commit 4d38bff053768a7edb68cb6b6ed6a45d3c20cd4a

Fail the build when Python with Mercurial is not available

Makefile
@@ -14,6 +14,9 @@ build:
 		PYTHON=python2 ; \
 	elif python -c 'import mercurial' 2> /dev/null ; then \
 		PYTHON=python ; \
+	else ; \
+		echo 'Python with Mercurial not available' >&2 ; \
+		exit 1 ; \
 	fi ; \
 	mkdir -p bin ; \
 	for s in git-remote-hg git-hg-helper ; do \

commit dae102ce7d6bc21ccdc113d11824cf4748973b23

Fix syntax error caused by extra semicolon
Fixes: commit 4d38bff053768a7edb68cb6b6ed6a45d3c20cd4a

Makefile
@@ -14,7 +14,7 @@ build:
 		PYTHON=python2 ; \
 	elif python -c 'import mercurial' 2> /dev/null ; then \
 		PYTHON=python ; \
-	else ; \
+	else \
 		echo 'Python with Mercurial not available' >&2 ; \
 		exit 1 ; \
 	fi ; \